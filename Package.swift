619f3480009074f4b96f78a81aa3fa568e78a5e7
Build Er <builder@macmini6.local>
2023-02-16 23:53:15 -0300

    Package version: 1.0.0-beta+20230217024255

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "YAJL",
+	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "YAJL",
+			targets: ["YAJLWrapper"])
+	],
+	dependencies: [
+		
+	],
+	targets: [
+		.target(name: "YAJLWrapper",
+				dependencies: [
+					"YAJL",
+					
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "YAJL",
+			url: "https://pkgs.genexus.dev/iOS/beta/YAJL-1.0.0-beta+20230217024255.xcframework.zip",
+			checksum: "f765ad7c7c546fa1e8e4a4bf914d085b1ff0954ef4ccf8e89eeb8b470c213546"
+		)
+	]
+)
