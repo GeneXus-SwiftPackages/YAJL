4ebb119123d1121220cadd97a887ec6a62ad3095
Build Er <builder@macmini6.local>
2023-03-02 08:43:00 -0300

    Package version: 1.0.0-beta+20230302113401

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "YAJL",
-			url: "https://pkgs.genexus.dev/iOS/beta/YAJL-1.0.0-beta+20230228111720.xcframework.zip",
-			checksum: "bd2ad1982cffc6fbe4fc392861e839f6b09f6003da13c85281b53611afbf79f7"
+			url: "https://pkgs.genexus.dev/iOS/beta/YAJL-1.0.0-beta+20230302113401.xcframework.zip",
+			checksum: "19052568fdd5fdb651d101bec986e824d950605e3d2bb8d97b2007fcd3e923b3"
 		)
 	]
 )
