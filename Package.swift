c0263acdc2b6a304c203e5c6b64af5fc73ead584
genexusbot <builder@genexus.com>
2023-07-14 16:27:35 -0300

    Package version: 1.0.0-preview.20230714192150

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "YAJL",
+	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "YAJL",
+			targets: ["YAJLWrapper"])
+	],
+	dependencies: [
+		
+	],
+	targets: [
+		.target(name: "YAJLWrapper",
+				dependencies: [
+					"YAJL",
+					
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "YAJL",
+			url: "https://pkgs.genexus.dev/iOS/preview/YAJL-1.0.0-preview.20230714192150.xcframework.zip",
+			checksum: "e3218fc5381e46c3fb6290b23cef5e7e0d277c161acf2203ecee652dd439dd1c"
+		)
+	]
+)
