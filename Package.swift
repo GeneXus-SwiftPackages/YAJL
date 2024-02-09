da4301dbf9e77a2e9c8703ee84c6b719605ab7b0
genexusbot <builder@genexus.com>
2024-02-09 15:31:45 -0300

    Package version: 1.4.26

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
 	name: "YAJL",
-	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	platforms: [.iOS("12.0"), .watchOS("9.0"), .tvOS("12.0")],
 	products: [
 		.library(
 			name: "YAJL",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "YAJL",
-			url: "https://pkgs.genexus.dev/iOS/releases/YAJL-1.1.0.xcframework.zip",
-			checksum: "b8d5507295ec23d18e047159b5a449091b0b45d2409b682cf609ba3f289a35a9"
+			url: "https://pkgs.genexus.dev/iOS/releases/YAJL-1.4.26.xcframework.zip",
+			checksum: "57cb4e85e6ceda5dfe8bc3fbf2749259ae064084250a44ad09100c98246cef36"
 		)
 	]
 )
