a0ed3f67824b162fea95e33cfb19598e39ed58a4
genexusbot <builder@genexus.com>
2024-04-09 14:24:01 -0300

    Package version: 1.5.0-rc.28

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "YAJL",
-			url: "https://pkgs.genexus.dev/iOS/preview/YAJL-1.5.0-rc.26.xcframework.zip",
-			checksum: "af763eebcf7b8a753e825b82f9e827987c0c7a022df29f903db4c91af70d5157"
+			url: "https://pkgs.genexus.dev/iOS/preview/YAJL-1.5.0-rc.28.xcframework.zip",
+			checksum: "c5d16e2a665f0eba1ea009e5f8038c0e1cc293a83d9f7e05cbedaf873050653c"
 		)
 	]
 )
