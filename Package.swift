928a51927cc2dd1cf19540f3f0b04092f0d52f70
Build Er <builder@macmini6.local>
2024-04-11 18:05:41 -0300

    Package version: 1.5.11

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
 	name: "YAJL",
-	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	platforms: [.iOS("12.0"), .watchOS("9.0"), .tvOS("12.0")],
 	products: [
 		.library(
 			name: "YAJL",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "YAJL",
-			url: "https://pkgs.genexus.dev/iOS/releases/YAJL-1.1.0.xcframework.zip",
-			checksum: "b8d5507295ec23d18e047159b5a449091b0b45d2409b682cf609ba3f289a35a9"
+			url: "https://pkgs.genexus.dev/iOS/releases/YAJL-1.5.11.xcframework.zip",
+			checksum: "54559504516d4d7ee5c2089d87810d2e7ebb25b93dd69ae285fb3586f6aaed85"
 		)
 	]
 )
