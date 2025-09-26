a1e84628fda70a8326a370b1b00f7b015b434331
The iOS Team @ GeneXus <builder@genexus.com>
2025-09-26 07:01:02 -0400

    Package version: 3.2.0

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
 	name: "YAJL",
-	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	platforms: [.iOS("15.0"), .watchOS("10.0"), .tvOS("18.0"), .visionOS("2.0")],
 	products: [
 		.library(
 			name: "YAJL",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "YAJL",
-			url: "https://pkgs.genexus.dev/iOS/releases/YAJL-1.1.0.xcframework.zip",
-			checksum: "b8d5507295ec23d18e047159b5a449091b0b45d2409b682cf609ba3f289a35a9"
+			url: "https://pkgs.genexus.dev/iOS/releases/YAJL-3.2.0.xcframework.zip",
+			checksum: "b5ced8b292d7424a87a17ecac72f7a1ca00defc622125fac03a3ed509482926c"
 		)
 	]
 )
