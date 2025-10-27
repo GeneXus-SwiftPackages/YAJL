4f76c2dae730933a251fb6f9884721d00b63e17f
The iOS Team @ GeneXus <builder@genexus.com>
2025-10-27 05:06:20 -0400

    Package version: 4.0.2

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "YAJL",
-			url: "https://pkgs.genexus.dev/iOS/releases/YAJL-4.0.0.xcframework.zip",
-			checksum: "1681ba59a215b0b73dc3a874bde31f02cf62890108b48a42339ed7f820fd06c8"
+			url: "https://pkgs.genexus.dev/iOS/releases/YAJL-4.0.2.xcframework.zip",
+			checksum: "8cd8773da6c9fb36343957de837137edc2dec4ae37ad9e1d775cec117c1cec01"
 		)
 	]
 )
